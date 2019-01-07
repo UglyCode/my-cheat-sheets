b459b62f289e67c465378ac7e187000b3af68332
Anton Postnikov <appostnikov@gmail.com>
2019-01-07 17:49:58 +0300

    type conversions tests added

--- JS/index.js
+++ JS/index.js
@@ -1,5 +1,5 @@
 "use strict";
-import {primitive_String,primitive_Number,primitive_Boolean,primitive_null,primitive_undefined} from './primitive.js';
+import {PRIMITIVE} from './primitive.js';
 
-console.log(`Hello world! ${primitive_String}`);
+console.log(`\n ======================= End of main script! =======================\n ${PRIMITIVE}`);
 
--- JS/primitive.js
+++ JS/primitive.js
@@ -1,11 +1,55 @@
 "use strict";
 console.log('======================= Primitive module load =======================');
 
-let primitive_String = 'string';
-let primitive_Number = 31;
-let primitive_Boolean = true;
-let primitive_null = null;
-let primitive_undefined = undefined;
+const PRIMITIVE = new Map();
 
-export {primitive_String,primitive_Number,primitive_Boolean,primitive_null,primitive_undefined};
+PRIMITIVE.set('string', 'just a string');
+PRIMITIVE.set('string_space', '   ');
+PRIMITIVE.set('string_empty', '');
+PRIMITIVE.set('numeric', 11);
+PRIMITIVE.set('numeric_zero', 0);
+PRIMITIVE.set('numric_nan', NaN);
+PRIMITIVE.set('boolean_true', true);
+PRIMITIVE.set('boolean_false', false);
+PRIMITIVE.set('undefined', undefined);
+PRIMITIVE.set('null', null);
+PRIMITIVE.set('symbol', Symbol('sym'));
+PRIMITIVE.set('symbol_key', Symbol.for('key'));
 
+export {PRIMITIVE};
+
+// Convert tests
+let mapToString = new Map();
+let mapToNumeric = new Map();
+let mapToBoolean = new Map();
+
+PRIMITIVE.forEach((value, key) => {
+    try {
+        mapToString.set(key, String(value));
+    } catch (err) {
+        mapToString.set(key, err);
+    }
+    try {
+        mapToNumeric.set(key, Number(value));
+    } catch (err) {
+        mapToNumeric.set(key, err);
+    }
+    try {
+        mapToBoolean.set(key, Boolean(value));
+    } catch (err) {
+        mapToBoolean.set(key, err);
+    }
+});
+
+console.log(mapToString,mapToNumeric,mapToBoolean);
+
+//interesting cases
+
+console.log(`1 + 1 + '1' + 1 + true => `, 1 + 1 + '1' + 1 + true);
+console.log(`!(1 + 1 + '1' + 1 + true) => `, !(1 + 1 + '1' + 1 + true));
+
+console.log(`1 + '1' + !!(1+false) + !!'0' => `, 1 + '1' + !!(1+false) + !!'0');
+console.log(`1 + '1' + !!(1+false) + !!'0' - 5 => `, 1 + '1' + !!(1+false) + !!'0' - 5);
+
+console.log(`1 + !!(1+false) + !!'0' + '1' => `, 1 + !!(1+false) + !!'0' + '1');
+console.log(`1 + !!(1+false) + !!'0' + '1' - 5 => `, 1 + !!(1+false) + !!'0' + '1' - 5);

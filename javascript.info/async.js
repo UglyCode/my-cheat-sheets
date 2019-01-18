e4d3493c9a8c43f5667765d2c90156aa24f5bbe7
UglyCode <appostnikov@gamil.com>
2019-01-18 14:22:00 +0000

    async modules updated

--- javascript.info/async.js
+++ javascript.info/async.js
@@ -1,39 +1,57 @@
 'use strict';
-function delay(ms) {
-    return new Promise((res, rej) =>{
-        setTimeout(res,ms);
-    });
-}
+// function delay(ms) {
+//     return new Promise((res, rej) =>{
+//         setTimeout(res,ms);
+//     });
+// }
+//
+// delay(3000).then(() => console.log('runs after 3 seconds'));
+//
+//
+// new Promise(function(resolve, reject) {
+//         throw new Error("Whoops!");
+//     })
+//
+//     .catch(function(error) { // (*)
+//         if (error instanceof URIError) {
+//             // handle it
+//         } else {
+//             console.log("Can't handle such error");
+//  //           throw error; // throwing this or another error jumps to the next catch
+//         }
+//     })
+//
+//     .then(()=> bla())
+//
+//
+//
+//     .then(function() {
+//         console.log('2');
+//     })
+//
+//     .catch(error => { // (**)
+//         console.log(`The unknown error has occurred: ${error}`);
+//         // don't return anything => execution goes the normal way
+//     })
+//
+//     .then(()=>console.log('3'))
+//
+//     .then(()=>console.log('4'));
+//
+//
+// new Promise(function(resolve, reject) {
+//     setTimeout(() => {
+//         throw new Error("err1!");
+//     }, 5000);
+//     throw new Error("err2!");
+// })
+//     .catch(console.log)
+//
+//     .then(()=>{
+//         setTimeout(() => {
+//             throw new Error("er3!");
+//         }, 1000);
+//     })
+//
+//     .catch(console.log);
 
-delay(3000).then(() => console.log('runs after 3 seconds'));
-
-
-new Promise(function(resolve, reject) {
-        throw new Error("Whoops!");
-    })
-
-    .catch(function(error) { // (*)
-        if (error instanceof URIError) {
-            // handle it
-        } else {
-            console.log("Can't handle such error");
- //           throw error; // throwing this or another error jumps to the next catch
-        }
-    })
-
-    .then(()=> bla())
-
-
-
-    .then(function() {
-        console.log('2');
-    })
-
-    .catch(error => { // (**)
-        console.log(`The unknown error has occurred: ${error}`);
-        // don't return anything => execution goes the normal way
-    })
-
-    .then(()=>console.log('3'))
-
-    .then(()=>console.log('4'));
---
+++ javascript.info/asyncFetch.js
@@ -0,0 +1,48 @@
+'use strict';
+let urls = [
+    'https://api.github.com/users/iliakan',
+    'https://api.github.com/users/remy',
+    'http://no-such-url'
+];
+
+Promise.all(urls.map((url)=>{
+    return fetch(url)
+        .catch(promosifyError);
+}))
+    .then(responses => {
+        // 3 urls => 3 array members
+        console.log('Logged: ' + responses[0].status); // 200
+        console.log('Logged: ' + responses[1].status); // 200
+        console.log('Logged: ' + responses[2]); // TypeError: failed to fetch (text may vary)
+    });
+
+//
+// let urls2 = [
+//     'https://api.github.com/users/iliakan',
+//     'https://api.github.com/users/remy',
+//     'https://api.github.com/users/jeresig'
+// ];
+
+// make fetch requests
+Promise.all(urls.map(url => {
+    return fetch(url)
+        .catch(promosifyError)
+    }
+))
+// map each response to response.json()
+    .then(responses => Promise.all(
+        responses.map(r => {
+            return r.json()
+                .catch(promosifyError)
+        })
+    ))
+    // show name of each user
+    .then(users => {  // (*)
+        for(let user of users) {
+            console.log('logged2: ' + user.name);
+        }
+    });
+
+function promosifyError(err){
+   return Promise.resolve(err);
+}

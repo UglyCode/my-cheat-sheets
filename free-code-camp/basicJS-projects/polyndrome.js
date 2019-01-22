61a9de79e7e07264f1485abe5086f44afa7ff466
UglyCode <appostnikov@gamil.com>
2019-01-22 14:49:22 +0000

    polindrome from #freeCodeCamp task solved

---
+++ free-code-camp/basicJS-projects/polyndrome.js
@@ -0,0 +1,16 @@
+function palindrome(str) {
+
+    for (let i =0; i<Math.floor(str.length/2); i++){
+        if(str[i]!==str[str.length-1-i]){
+            return false;
+        }
+    }
+
+    return true;
+}
+
+
+
+console.log(palindrome("euyue"));
+console.log(palindrome("jopa"));
+console.log(palindrome("ollo"));

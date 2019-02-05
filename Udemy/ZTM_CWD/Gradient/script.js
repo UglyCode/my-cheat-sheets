e2903973726342746aa4a6bb31082ca6956c8072
UglyCode <appostnikov@gamil.com>
2019-02-05 12:47:01 +0000

    super cool Background picker updated

--- Udemy/ZTM_CWD/Gradient/script.js
+++ Udemy/ZTM_CWD/Gradient/script.js
@@ -2,6 +2,7 @@ let output = document.getElementById('output');
 let color1 = document.getElementById('color1');
 let color2 = document.getElementById('color2');
 let body =  document.getElementById('gradient');
+let randomButton = document.getElementById('random');
 
 
 console.log(output);
@@ -14,6 +15,23 @@ function changeBackground(){
     output.textContent = localGrafient+';';
 }
 
+function generateRandomColors(){
+    color1.value = pickRandomColor();
+    color2.value = pickRandomColor();
+    changeBackground();
+}
+
+function pickRandomColor(){
+    return '#'+rndRGB()+rndRGB()+rndRGB();
+}
+
+function rndRGB(){
+    return Math.floor(Math.random()*255).toString(16);
+}
 
 color1.addEventListener('input',changeBackground);
 color2.addEventListener('input',changeBackground);
+randomButton.addEventListener('click',generateRandomColors);
+
+
+changeBackground();

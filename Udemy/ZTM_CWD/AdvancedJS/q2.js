304f9c4bfc96ce45cb37c4000d49c5e73ffb769f
Anton Postnikov <appostnikov@gmail.com>
2019-02-08 22:12:32 +0300

    task 2 solution added

---
+++ Udemy/ZTM_CWD/AdvancedJS/q2.js
@@ -0,0 +1,48 @@
+let arr = [1,2,4,591,392,391,2,5,10,2,1,1,1,20,20];
+
+
+//recursion
+function findAdded(arr, target){
+
+    let currElem = arr.pop();
+    if (!currElem) return 'no matches';
+
+    for (i=0; i<arr.length; i++) {
+        let elem = arr[i];
+       if ((elem + currElem) === target) {
+           return [elem, currElem];
+       }
+    }
+    return findAdded(arr, target);
+
+}
+
+//no recursion with cache
+function findAddedNoRec(arr, target){
+
+    const length = arr.length;
+    for (let i=0; i<length; i++) {
+
+        let cache = new Set();
+        let currElem = arr.pop();
+
+        if (!currElem) return 'no matches';
+
+        for (let j = 0; j < arr.length; j++) {
+
+            let elem = arr[j];
+            if (cache.has(elem)) continue;
+
+            if ((elem + currElem) === target) {
+                return [elem, currElem];
+            }
+
+            cache.add(elem);
+        }
+    }
+    return findAdded(arr, target);
+
+}
+
+console.log(findAddedNoRec(arr, 393));
+console.log(findAddedNoRec(arr, 501));

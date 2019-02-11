65abcd83e4d49ff5947f992a149dd7eec7c97b30
uglyCode <appostnikov@gmail.com>
2019-02-11 22:52:27 +0300

    robofrriends v1

--- Udemy/ZTM_CWD/React/robofriends/src/Card.js
+++ Udemy/ZTM_CWD/React/robofriends/src/Card.js
@@ -1,14 +1,14 @@
 import React, {components} from 'react';
 
-const Card = () => {
+const Card = ({name, email, id}) => {
 
     return (
 
-        <div className='bg-light-green dib br3 pa3 ma2 grow bw-2 shadow-5-l'>
-            <img src = 'https://robohash.org/test?200x200' alt = 'robots'/>
+        <div className='bg-light-green dib br3 pa3 ma2 grow bw-2 shadow-5-l tc'>
+            <img src = {`https://robohash.org/${id}?200x200`} alt = 'robots'/>
             <div>
-                <h2>Appostnikov</h2>
-                <p>appostnikov@gmail.com</p>
+                <h2>{name}</h2>
+                <p>{email}</p>
             </div>
         </div>
 

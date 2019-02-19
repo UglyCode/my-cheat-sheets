2d893c8130e761d380b6180615408bee2d437ca8
uglyCode <appostnikov@gmail.com>
2019-02-19 22:03:16 +0300

    clarifi API components added

--- Udemy/ZTM_CWD/face-recon/src/components/Logo/Logo.js
+++ Udemy/ZTM_CWD/face-recon/src/components/Logo/Logo.js
@@ -9,7 +9,7 @@ const Logo = () =>{
 
     return(
         <div className='ma4 mt0'>
-            <Tilt className="Tilt br2 shadow-2" options={{ max : 50 }} style={{ height: 100, width: 100 }} >
+            <Tilt className="Tilt br2 shadow-3" options={{ max : 50 }} style={{ height: 100, width: 100 }} >
                 <div className="Tilt-inner pa3">
                     <img src={brain} alt='brain' style={{paddingTop: '5px'}}/>
                 </div>

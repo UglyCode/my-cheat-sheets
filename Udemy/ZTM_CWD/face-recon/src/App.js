14fab4f27835b16ad5ac82457a78a295f6f81da3
uglyCode <appostnikov@gmail.com>
2019-02-20 23:32:44 +0300

    face recon added

--- Udemy/ZTM_CWD/face-recon/src/App.js
+++ Udemy/ZTM_CWD/face-recon/src/App.js
@@ -14,7 +14,6 @@ const app = new Clarifai.App({
     apiKey: '2c149afabd8141448623c02e728db777'
 });
 
-
 const particlesOptions = {
     particles: {
         number: {
@@ -33,13 +32,37 @@ const particlesOptions = {
     }
 };
 
+const parceClarifaiAnswer = (response) =>{
+    return response.outputs[0].data.regions.map((elem)=>{
+
+    });
+};
+
+const parseBoundingBox = (width, height, boxObj) => {
+    console.log(width, height, boxObj);
+    let box = {
+        top: height * boxObj.top_row,
+        left: width * boxObj.left_col,
+        bottom: height * (1 - boxObj.bottom_row),
+        right: width * (1 - boxObj.right_col)
+    };
+    console.log(box);
+    return box;
+
+};
 
 class App extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            input: ''
+            input: '',
+            boundingBoxes: [{
+                top_row: 0,
+                left_col: 0,
+                bottom_row: 0,
+                right_col: 0
+            }]
         }
     }
 
@@ -49,16 +72,28 @@ class App extends Component {
 
     onSubmit = () =>{
       console.log('gooo');
-        app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
-            .then(
-            function(response) {
+      app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+            .then((response) => {
                 console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
-            },
-            function(err) {
-                // there was an error
+                this.setState({boundingBoxes: this.calcBoxes(response.outputs[0].data.regions)});
+            })
+            .catch(function(err) {
+                console.log(err);
             }
         );
     };
+
+    calcBoxes(clarifaiRegions){
+        console.log(clarifaiRegions);
+        return clarifaiRegions.map((elem)=>{
+            let box = elem.region_info.bounding_box;
+            box.top_row *= 100;
+            box.left_col *= 100;
+            box.bottom_row = (1-box.bottom_row) *100;
+            box.right_col = (1-box.right_col) * 100;
+            return box;
+        })
+    }
 
     render() {
     return (
@@ -66,16 +101,14 @@ class App extends Component {
           <Particles className='particles'
               params={particlesOptions}
           />
-        <div>
-            <Navigation />
-            <Logo />
-            <Rank />
-        </div>
-        <ImageLinkForm
-            onInputChange={this.onInputChange}
-            onSubmit={this.onSubmit}
-        />
-        <FaceRecognition imgLink={this.state.input}/>
+          <Navigation />
+          <Logo />
+          <Rank />
+            <ImageLinkForm
+                onInputChange={this.onInputChange}
+                onSubmit={this.onSubmit}
+            />
+        <FaceRecognition imgLink={this.state.input} imgBox={this.state.boundingBoxes[0]}/>
       </div>
     );
   }
--- Udemy/ZTM_CWD/face-recon/src/components/FaceRecognition/FaceRecognition.js
+++ Udemy/ZTM_CWD/face-recon/src/components/FaceRecognition/FaceRecognition.js
@@ -1,12 +1,24 @@
 import React from 'react';
+import './FaceRecognition.css';
 
-const FaceRecognition = ({imgLink}) =>{
-
+const FaceRecognition = ({imgLink, imgBox}) =>{
+    console.log(imgBox,imgBox.top_row,imgBox.left_col, imgBox.bottom_row, imgBox.right_col);
     return(
         <div className='centerFlex'>
-            <img alt='your image' src={imgLink} style={{width: '500px', height: 'auto'}}/>
+            <div style={{width: '500px', position: 'relative'}}>
+                <img id='inputImg' alt='your image' src={imgLink} style={{width: '100%', height: 'auto'}}/>
+                <div className='bounding-box'
+                     style={{
+                         top: `${imgBox.top_row}%`,
+                         right: `${imgBox.right_col}%`,
+                         bottom: `${imgBox.bottom_row}%`,
+                         left: `${imgBox.left_col}%`}}>
+
+                </div>
+            </div>
         </div>
     );
 };
 
 export default FaceRecognition;
+

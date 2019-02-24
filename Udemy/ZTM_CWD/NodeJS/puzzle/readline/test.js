31a513c8ecbd98337b802e744d4aa83b6caa5fd8
uglyCode <appostnikov@gmail.com>
2019-02-24 08:30:49 +0300

    number guessing problem solved

---
+++ Udemy/ZTM_CWD/NodeJS/puzzle/readline/test.js
@@ -0,0 +1,72 @@
+var readline = require('readline');
+var params = {
+    cases: NaN,
+    min: NaN,
+    max: NaN,
+    guesses: NaN,
+    currValue: NaN
+};
+
+var rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.on('line', (input) => {
+    if (isNaN(params.cases)) {
+        params.cases = Number(input);
+        return;
+    } //first step
+
+    if (isNaN(params.min)){
+        var arrMinMax = input.split(' ');
+        params.min = Number(arrMinMax[0]);
+        params.max = Number(arrMinMax[1]);
+        return;
+    } //second step
+
+    if (isNaN(params.guesses)){
+        params.guesses = Number(input);
+        makeGuess();
+        return;
+    } //third step
+
+    // evaluate the judge
+    switch (input) {
+        case 'TOO_SMALL':
+            params.min = params.currValue;
+            makeGuess();
+            break;
+        case 'TOO_BIG':
+            params.max = params.currValue;
+            makeGuess();
+            break;
+        case 'CORRECT':
+            prepareToNextCase();
+            if (params.cases > 0) break;
+        default:
+            exit();
+    }
+});
+
+function makeGuess() {
+    if (!params.guesses) exit();
+
+    params.currValue = Math.ceil((params.max - params.min)/2 + params.min);
+    rl.setPrompt(""+params.currValue);
+    rl.prompt();
+    params.guesses -= 1;
+}
+
+function prepareToNextCase() {
+    params.cases -= 1;
+    params.min = NaN;
+    params.max = NaN;
+    params.guesses = NaN;
+    params.currValue = NaN;
+}
+
+function exit() {
+    rl.close();
+    process.exit(0);
+}
--- Udemy/ZTM_CWD/NodeJS/puzzle/solving.js
+++ Udemy/ZTM_CWD/NodeJS/puzzle/solving.js
@@ -4,8 +4,20 @@ fs.readFile(`${__dirname}/input.txt`, (err, data) => {
     const dataString = data.toString('utf8');
 
     console.time('floor');
-    console.log(dataString.length - 2*dataString.match(/\)/g).length);
+    console.log('floor', dataString.length - 2*dataString.match(/\)/g).length);
     console.timeEnd('floor');
+
+    console.time('floor2');
+    const directionsArray = dataString.split('');
+    const answer = directionsArray.reduce((acc, curr)=>{
+        if (curr==='('){
+            return ++acc;
+        } else{
+            return --acc;
+        }
+    }, 0);
+    console.log('floor2', answer);
+    console.timeEnd('floor2');
 
     console.time('basement');
     let floor = 0;

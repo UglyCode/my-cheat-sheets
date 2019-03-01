9cc61da1870226d08eb3bed845d7ab8fbfe65055
uglyCode <appostnikov@gmail.com>
2019-03-01 22:58:25 +0300

    serverside PSQL database interaction added

--- Udemy/ZTM_CWD/fece-recon-api/server.js
+++ Udemy/ZTM_CWD/fece-recon-api/server.js
@@ -2,6 +2,18 @@ const express = require('express');
 const bp = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
+const knex = require('knex')
+
+const pg = knex({
+   client: 'pg',
+   connection: {
+      host : '127.0.0.1',
+      user : 'postgres',
+      password : 'kloop12',
+      database : 'face-recon'
+   }
+});
+
 
 const app = express();
 app.use(bp.json());
@@ -34,58 +46,66 @@ app.get('/', (req, res)=>{
 
 
 app.post('/signIn', (req, res) =>{
-   if (req.body.email === database.users[0].email &&
-       req.body.password === database.users[0].password){
-      res.json(database.users[0]);
-   } else {
-      res.status(400).json('invalid credentials');
-   }
-
+   pg.select('email', 'hash').from('login')
+       .where('email', '=', req.body.email)
+       .then( userData => {
+          if (bcrypt.compareSync(req.body.password, userData[0].hash)){
+             return pg.select('*').from('users').
+                 where('email', '=', req.body.email)
+                 .then(user => res.json(user[0]))
+                 .catch(err => res.status('400').json('oy-oy-oy-oy'))
+          } else {
+             res.status('400').json('wrong email or password!')
+          }
+       })
+       .catch(err => res.status('400').json('wrong credentials'));
 });
 
 app.post('/register', (req,res)=>{
    const {email, name, password} = req.body;
-   bcrypt.hash(password, null, null, function(err, hash) {
-      console.log(hash)
-   });
-   database.users.push({
-      id: '125',
-      name: name,
-      email: email,
-      password: password,
-      entries: 0,
-      joined: new Date()
-   });
-   res.json(database.users[database.users.length-1]);
+   const hash = bcrypt.hashSync(password);
+
+   pg.transaction(trx => {
+      trx.insert({
+         hash: hash,
+         email: email
+      })
+          .into('login')
+          .returning('email')
+          .then(loginEmail => {
+               return trx('users')
+                   .returning('*')
+                   .insert({
+                     email: loginEmail[0],
+                     name: name,
+                     joined: new Date()})
+                   .then(userData => res.json(userData[0]))
+          })
+          .then(trx.commit)
+          .catch(trx.rollback)
+      })
+       .catch(err => res.status('400').json('no U'));
 });
 
 app.get('/profile/:id', (req, res)=>{
    const {id} = req.params;
-   let found = false;
-   database.users.forEach((user)=>{
-      if (user.id === id){
-         found = true;
-         res.json(user);
-      }
-   });
-
-   if (!found){
-      res.status(404).json('user not found in database');
-   }
+   pg.select('*')
+       .from('users')
+       .where({id})
+       .then(user => {
+          user[0] ? res.json(user[0]) : res.status('404').json('not found');
+       })
+       .catch(err => res.status('404').json('error'));
 });
 
 app.put('/image', (req, res) => {
    const {id} = req.body;
-   let found = false;
-   database.users.forEach((user)=>{
-      if (user.id === id){
-         found = true;
-         return res.json(++user.entries);
-      }
-   });
-   if (!found){
-      res.status(404).json('user not found in database');
-   }
+   pg('users')
+       .where('id','=', id)
+       .increment('entries', 1)
+       .returning('entries')
+       .then(entries => res.json(entries))
+       .catch(err => res.status('400').json("can't increment your entries, sorry."));
 });
 
 

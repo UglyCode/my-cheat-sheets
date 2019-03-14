14739d80b07d7c3d7e49b4b0295788fc85adff73
uglyCode <appostnikov@gmail.com>
2019-03-14 22:39:56 +0300

    new types added

--- Udemy/ZTM_JTS/TypeScript/first.js
+++ Udemy/ZTM_JTS/TypeScript/first.js
@@ -1,3 +1,49 @@
 var sum = function (a, b) {
     return a + b;
 };
+var s = sum(4, 5);
+var n = sum(4, 6);
+console.log(s);
+//boolean
+var isWeb = true;
+//number
+var age = 21;
+//string
+var newName = "q" + s;
+//Array
+var films = ['mib', 'sleepy hollow'];
+var foob = ['wine', 'burger'];
+//objects
+var coolGuy = {
+    name: 'anton'
+};
+//null
+var whoa = undefined;
+var yaaya = null;
+// Tuple
+var fusion;
+fusion = ['money', 10000000];
+//Enum
+var Sizw;
+(function (Sizw) {
+    Sizw[Sizw["small"] = 1] = "small";
+    Sizw[Sizw["medium"] = 2] = "medium";
+    Sizw[Sizw["large"] = 3] = "large";
+})(Sizw || (Sizw = {}));
+var size = Sizw[2];
+var size2 = Sizw.small;
+//Any
+//void
+var laugh = function () {
+    console.log('lalala');
+};
+var sayLala = function () {
+    return 'lalala';
+};
+//never
+var error = function () {
+    throw Error('oopss');
+};
+var msuculusDescribeGirl = function (girl) {
+    console.log(girl);
+};
--- Udemy/ZTM_JTS/TypeScript/first.ts
+++ Udemy/ZTM_JTS/TypeScript/first.ts
@@ -3,5 +3,64 @@ const sum = (a:number, b:number) => {
 };
 
 const s = sum(4, 5);
-
+const n = sum(4, 6);
 console.log(s);
+
+
+//boolean
+let isWeb: boolean = true;
+//number
+let age: number = 21;
+//string
+let newName: string = `q${s}`;
+//Array
+let films: string[] = ['mib','sleepy hollow'];
+let foob: Array<string> = ['wine', 'burger'];
+//objects
+let coolGuy: object = {
+  name: 'anton'
+};
+//null
+let whoa:undefined = undefined;
+let yaaya:null = null;
+
+// Tuple
+let fusion: [string, number];
+fusion = ['money', 10000000];
+
+//Enum
+enum Sizw {
+    small = 1, medium = 2, large=3
+}
+
+let size: string = Sizw[2];
+let size2: number = Sizw.small;
+
+
+//Any
+
+
+//void
+let laugh = (): void =>{
+  console.log('lalala')
+};
+
+let sayLala = (): string =>{
+    return 'lalala';
+};
+
+//never
+let error = ():never => {
+  throw Error('oopss');
+};
+
+//interface
+interface GirlArmy {
+    count: number,
+    boobs: 'string',
+    fuckeble: boolean
+}
+
+let msuculusDescribeGirl = (girl: GirlArmy) => {
+  console.log(girl);
+};
